e845bc90a378e8f8748c8ace811ef1728beeb237
Yerfdog1990 <gouma308@gmail.com>
2024-10-24 18:56:03 +0700

    Reflection of fields in Java.

--- ObjectClass/src/ReflectionOfFields/Dog.java
+++ ObjectClass/src/ReflectionOfFields/Dog.java
@@ -9,10 +9,12 @@ Like methods, we can also inspect and modify different fields of a class using t
  */
 public class Dog {
     public String type;
+    private String color;
 }
 
 class Main {
     public static void main(String[] args) {
+        //Access and modify public fields
         try {
             // create an object of Dog
             Dog d1 = new Dog();
@@ -36,6 +38,39 @@ class Main {
             String modifier1 = Modifier.toString(mod);
             System.out.println("Modifier: " + modifier1);
             System.out.println(" ");
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Access and modify private fields
+        try {
+            // create an object of Dog
+            Dog d1 = new Dog();
+
+            // create an object of Class
+            // using getClass()
+            Class obj = d1.getClass();
+
+            // access the private field color
+            Field field1 = obj.getDeclaredField("color");
+
+            // allow modification of the private field
+            field1.setAccessible(true);
+
+            // set the value of color
+            field1.set(d1, "brown");
+
+            // get the value of field color
+            String colorValue = (String) field1.get(d1);
+            System.out.println("Value: " + colorValue);
+
+            // get the access modifier of color
+            int mod2 = field1.getModifiers();
+
+            // convert the access modifier to string
+            String modifier2 = Modifier.toString(mod2);
+            System.out.println("Modifier: " + modifier2);
         }
 
         catch (Exception e) {
