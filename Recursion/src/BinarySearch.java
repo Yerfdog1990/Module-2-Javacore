520d7637da96d77f0ef6655f8a4238afd7b13f52
Yerfdog1990 <gouma308@gmail.com>
2024-11-01 05:06:27 +0700

    Binary search using iteration and recursion methods.

--- Recursion/src/BinarySearch.java
+++ Recursion/src/BinarySearch.java
@@ -1,26 +1,50 @@
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-        int target = 70;
-        int result = performBinarySearch(array, target, 0, array.length-1);
-        if(result == -1){
-            System.out.println("Target number not found");
+        int[] array = {13, 23, 34, 39, 40, 41, 51, 67, 79, 80, 91};
+        int target = 91;
+        int low = 0;
+        int high = array.length - 1;
+        int binaryResult = recursiveBinarySearch(array, target, low, high);
+        if(binaryResult == -1){
+            System.out.printf("Sorry, %d does not exist. Try again.", target);
         }else{
-            System.out.println("Target number is located at index: " +result);
+            System.out.printf("Binary search successful! %d is located are index %d.", target, binaryResult);
+        }
+        int iterativeResult = iterativeBinarySearch(array, 99, 0, array.length-1);
+        if(binaryResult == -1){
+            System.out.printf("\nSorry, %d does not exist. Try again.", target);
+        }else{
+            System.out.printf("\nBinary search successful! %d is located are index %d.", target, iterativeResult);
         }
     }
-      private static int performBinarySearch(int[] array, int target, int low, int high){
-          if(low > high){
-              return -1;
-          }
-          int mid = (low + high)/2;
-          if(array[mid] == target){
-              return mid;
-          }
-          if(array[mid] > target){
-              return performBinarySearch(array, target, low, mid-1);
-          }else{
-              return performBinarySearch(array, target, mid+1, high);
-          }
-      }
+    //Binary search using recursion
+    static int recursiveBinarySearch(int[] array, int target, int low, int high){
+        while(low <= high){
+            return -1;
+        }
+        int mid = (high + low)/2;
+        if(array[mid] == target){
+            return mid;
+        }
+        if(array[mid] > target){
+            return recursiveBinarySearch(array, target, low, mid-1);
+        }else{
+            return recursiveBinarySearch(array, target, mid+1, high);
+        }
+    }
+    //Binary search using iteration
+    static int iterativeBinarySearch(int[] array, int target, int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (array[mid] == target) {
+                return mid;
+            }
+            if (array[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
