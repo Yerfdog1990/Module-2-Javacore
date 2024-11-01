41e59f121cee4a097444cfc3e7e61ab9d7def966
Yerfdog1990 <gouma308@gmail.com>
2024-11-01 19:21:51 +0700

    Prime numbers

---
+++ Recursion/src/PrimeNumbers.java
@@ -0,0 +1,17 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+     findPrimaNumbers(132);
+    }
+    static void findPrimaNumbers(int n){
+        if(n <= 1){
+            return; 
+        }
+        for (int i = 2; i <= n; i++) {
+            if(n % i == 0){
+                System.out.print(i+ ", ");
+                findPrimaNumbers(n/i);
+                break;
+            }
+        }
+    }
+}
