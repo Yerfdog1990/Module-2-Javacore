c5569b840b2b92de53bf604bfd1e82970f393a4d
Yerfdog1990 <gouma308@gmail.com>
2024-11-08 04:36:18 +0700

    Creating a thread by extending Thread class

--- Multithreading/src/CreateThread/Method1.java
+++ Multithreading/src/CreateThread/Method1.java
@@ -13,9 +13,16 @@ public class Method1 extends Thread{
 class Main1{
     public static void main(String[] args) {
         int n = 8;
+        //Calling run() method using start() function
         for (int i = 0; i < n; i++) {
-            Method1 object = new Method1();
-            object.start();
+            Method1 object1 = new Method1();
+            object1.start(); //Threads have different IDs
+        }
+        int m = 8;
+        //Calling run() method using run() function
+        for (int i = 0; i < m; i++) {
+            Method1 object2 = new Method1();
+            object2.run(); //Threads have the same IDs
         }
     }
 }
