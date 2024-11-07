672bb3ca6a13d3e87a811a84a6b979b98c6ed285
Yerfdog1990 <gouma308@gmail.com>
2024-11-08 05:02:47 +0700

    Sleeping the main thread

---
+++ Multithreading/src/Thread/Sleep/MyThread.java
@@ -0,0 +1,18 @@
+package Thread.Sleep;
+
+// Java Program for sleeping the main thread.
+public class MyThread {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            try {
+                // it will sleep the main thread for 1 sec
+                // ,each time the for loop runs
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Thread " +i+ " is running.");
+        }
+    }
+}
