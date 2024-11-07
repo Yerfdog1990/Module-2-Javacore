0cb8de46d7e426a184f98d546da7a47463349e46
Yerfdog1990 <gouma308@gmail.com>
2024-11-07 18:23:42 +0700

    Naming threads using a parametrized constructor

---
+++ Multithreading/src/NamingThread/MyThread1.java
@@ -0,0 +1,29 @@
+package NamingThread;
+
+public class MyThread1 extends Thread{
+    private String name;
+
+    public MyThread1(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+ " is running...");
+    }
+    //Main method
+    public static void main(String[] args) {
+        //Creating thread
+        MyThread1 myThread1 = new MyThread1("Geek1");
+        MyThread1 myThread2 = new MyThread1("Geek2");
+
+        //Getting the name of the threads above.
+        System.out.println("Thread 1: " +myThread1.getName());
+        System.out.println("Thread 2: " +myThread2.getName());
+
+        //Staring threads using start() method
+        myThread1.start();
+        myThread2.start();
+
+    }
+}
