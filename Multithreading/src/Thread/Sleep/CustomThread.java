c863aeeb8d1fc702b67fa6e290d7000d62cd23db
Yerfdog1990 <gouma308@gmail.com>
2024-11-08 05:39:01 +0700

    Sleeping the custom thread

--- Multithreading/src/Thread/Sleep/CustomThread.java
+++ Multithreading/src/Thread/Sleep/CustomThread.java
@@ -1,20 +1,26 @@
 package Thread.Sleep;
 
+// Java Program for sleeping the custom thread.
 public class CustomThread extends Thread{
-    public void run(){
-        System.out.println(Thread.currentThread().getName()+ "running...");
+    public void run() {
+        System.out.println(Thread.currentThread().getName());//Thread 0
+       try{
+           for (int i = 0; i < 5; i++) {
+               // it will sleep the main thread for 1 sec
+               // ,each time the for loop runs
+               Thread.sleep(5000);
+               // This Thread.sleep() method will sleep the
+               // thread 0.
+               // printing the value of the variable
+               System.out.println("Custom thread " +i+ " is running...");
+           }
+       }catch (InterruptedException e) {
+        throw new RuntimeException(e);
+        }
     }
     //Main method
     public static void main(String[] args) {
         CustomThread object = new CustomThread();
-        int n = 5;
-        for (int i = 0; i < 5; i++) {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Thread " +i+ " is running ...");
-        }
+        object.start();
     }
 }
