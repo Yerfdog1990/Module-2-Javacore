20b922e53dd9a686447c1d24039adb3dc4328cbf
Yerfdog1990 <gouma308@gmail.com>
2024-11-12 20:35:53 +0700

    Lambda Expression with Zero parameter

--- Lambda/src/CreateLambdaExpression/School.java
+++ Lambda/src/CreateLambdaExpression/School.java
@@ -3,3 +3,20 @@ package CreateLambdaExpression;
 public interface School {
     void printName();
 }
+class Student implements School{
+    @Override
+    public void printName() {
+        System.out.println("\nImplemented School interface:");
+        System.out.println("My name is Godfrey.");
+    }
+
+    public static void main(String[] args) {
+        Student myName1 = new Student();
+        myName1.printName();
+        System.out.println("\nUsed lambda expression:");
+        School myName2 = ()-> System.out.println("My name is Godfrey.");
+        myName2.printName();
+    }
+}
+
+
