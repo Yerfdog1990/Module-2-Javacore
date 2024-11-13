d4e41516312a8c91afda65f606ae3358ff9994d4
Yerfdog1990 <gouma308@gmail.com>
2024-11-13 18:52:54 +0700

    Lambda expression with one parameter.

--- Lambda/src/CreateLambdaExpression/Citizenship.java
+++ Lambda/src/CreateLambdaExpression/Citizenship.java
@@ -4,8 +4,12 @@ public interface Citizenship {
     void printCountry(String name);
 }
 class countryOrigin{
+    static void print(Citizenship T, String country){
+        T.printCountry("Kenya");
+    }
     public static void main(String[] args) {
         Citizenship country = name -> System.out.println(name);
         country.printCountry("Kenya");
+        print(name -> System.out.println(name), "Kenya");
     }
 }
