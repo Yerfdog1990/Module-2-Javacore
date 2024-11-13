01b6a314d2b987fc4f22ed8ef8bb177dce2d99b5
Yerfdog1990 <gouma308@gmail.com>
2024-11-13 21:10:28 +0700

    Sorting a Tree Map using lambda expression.

--- Lambda/src/SortCollection/SortTreeMap.java
+++ Lambda/src/SortCollection/SortTreeMap.java
@@ -1,4 +1,20 @@
 package SortCollection;
 
+import java.util.Collections;
+import java.util.TreeMap;
+
 public class SortTreeMap {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>((Obj1, Obj2)-> Obj1.compareTo(Obj2));
+        treeMap.put("Godfrey", 34);
+        treeMap.put("John", 4);
+        treeMap.put("Cyril", 5);
+        treeMap.put("Peter", 34);
+        treeMap.put("Tom", 14);
+        treeMap.put("Abby", 6);
+
+        System.out.println(treeMap);
+
+
+    }
 }
