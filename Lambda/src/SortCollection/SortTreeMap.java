3b955ac5397d4e1876364e92bf7c6fe8110bcb0e
Yerfdog1990 <gouma308@gmail.com>
2024-11-14 04:34:56 +0700

    Sorting an TreeSet using lambda expression.

--- Lambda/src/SortCollection/SortTreeMap.java
+++ Lambda/src/SortCollection/SortTreeMap.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 
 public class SortTreeMap {
     public static void main(String[] args) {
-        TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>((Obj1, Obj2)-> Obj1.compareTo(Obj2));
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
         treeMap.put("Godfrey", 34);
         treeMap.put("John", 4);
         treeMap.put("Cyril", 5);
@@ -13,8 +13,18 @@ public class SortTreeMap {
         treeMap.put("Tom", 14);
         treeMap.put("Abby", 6);
 
+        //Prints based on natural order of alphabets
         System.out.println(treeMap);
 
+        TreeMap<Integer, String> treeMap1 = new TreeMap<>();
+        treeMap1.put(34,"Godfrey");
+        treeMap1.put(4,"John");
+        treeMap1.put(5,"Cyril");
+        treeMap1.put(34,"Peter");
+        treeMap1.put(14,"Tom");
+        treeMap1.put(6,"Abby");
 
+        //Prints based on natural order of numbers
+        System.out.println(treeMap1);
     }
 }
