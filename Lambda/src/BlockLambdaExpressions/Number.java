b514e3bb6f845e49ac5aa557e9fe80f614521d2e
Yerfdog1990 <gouma308@gmail.com>
2024-11-15 19:16:48 +0700

    Block lambda expression example 1

--- Lambda/src/BlockLambdaExpressions/Number.java
+++ Lambda/src/BlockLambdaExpressions/Number.java
@@ -7,6 +7,8 @@ class DriverClass{
     public static void main(String[] args) {
         // Lambda expression body
         Number isEven = (n)-> (n % 2) == 0;
+        // Above is lambda expression which tests
+        // passed number is even or odd
         if(isEven.checkEven(21))
             System.out.println("The number is even.");
         else
