5e79f2f788928de6a59077834eae790606bfbf81
Yerfdog1990 <gouma308@gmail.com>
2024-11-16 18:46:58 +0700

    Check if string has alphabets only using lambda expression and method referencing.

---
+++ Lambda/src/CheckIfStringHasAlphabet/AlphabetCheck.java
@@ -0,0 +1,14 @@
+package CheckIfStringHasAlphabet;
+
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class AlphabetCheck {
+    public static void main(String[] args) {
+        Predicate<String> isAlphabetOnly = string -> (string.chars().allMatch(Character::isLetter));
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("\nEnter text: ");
+        String text = userInput.nextLine();
+        System.out.println("The word " + "\""+text+ "\"" + " contains only alphabets: " + isAlphabetOnly.test(text));
+    }
+}
