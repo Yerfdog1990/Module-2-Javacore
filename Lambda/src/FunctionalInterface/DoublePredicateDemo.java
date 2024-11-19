1e78c43b65b2609127597fa73050874e8fb84dc7
Yerfdog1990 <gouma308@gmail.com>
2024-11-19 20:21:42 +0700

    BiPredictor functional interface.

--- Lambda/src/FunctionalInterface/DoublePredicateDemo.java
+++ Lambda/src/FunctionalInterface/DoublePredicateDemo.java
@@ -32,8 +32,19 @@ class DoubleSum{
                     return DoublePredicateDemo.sumIsEven(num1, num2);
                 }
             };
-            boolean result = checkIfEven1.test(firstDouble, secondDouble);
-            System.out.printf("Using anonymous class: The sum of %.2f and %.2f is even: %b\n", firstDouble, secondDouble, result);
+            boolean result1 = checkIfEven1.test(firstDouble, secondDouble);
+            System.out.printf("Using anonymous class: The sum of %.2f and %.2f is even: %b\n", firstDouble, secondDouble, result1);
+
+            //Check if sum of double values is even using lambda expression
+            BiPredicate<Double, Double> chekIfEven2 = (num1, num2) ->DoublePredicateDemo.sumIsEven(firstDouble, secondDouble);
+            boolean result2 = chekIfEven2.test(firstDouble, secondDouble);
+            System.out.printf("Using lambda expression: The sum of %.2f and %.2f is even: %b\n", firstDouble, secondDouble, result2);
+
+            //Check if sum of double values is even using method referencing
+            BiPredicate<Double, Double> chekIfEven3 = DoublePredicateDemo::sumIsEven;
+            boolean result3 = chekIfEven2.test(firstDouble, secondDouble);
+            System.out.printf("Using method referencing: The sum of %.2f and %.2f is even: %b\n", firstDouble, secondDouble, result2);
+
         }
     }
 }
