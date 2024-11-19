fadb57f2ed29ddb672f1c69c743b770ef1005077
Yerfdog1990 <gouma308@gmail.com>
2024-11-20 04:36:59 +0700

    BiPredictor functional interface.

--- Lambda/src/FunctionalInterface/BiPredicateDemo.java
+++ Lambda/src/FunctionalInterface/BiPredicateDemo.java
@@ -8,10 +8,9 @@ DoublePredicate
 Function: Variants of Predicate<T> that accept a primitive double as input.
 Methods: boolean test(double value)
  */
-public class DoublePredicateDemo {
+public class BiPredicateDemo {
     //Static method to check if the sum of two doubles is even
     public static boolean sumIsEven(double num1, double num2){
-
         return (num1 + num2) % 2 == 0.0;
     }
 }
@@ -29,22 +28,21 @@ class DoubleSum{
             BiPredicate<Double, Double> checkIfEven1 = new BiPredicate<>() {
                 @Override
                 public boolean test(Double num1, Double num2) {
-                    return DoublePredicateDemo.sumIsEven(num1, num2);
+                    return BiPredicateDemo.sumIsEven(num1, num2);
                 }
             };
             boolean result1 = checkIfEven1.test(firstDouble, secondDouble);
             System.out.printf("Using anonymous class: The sum of %.2f and %.2f is even: %b\n", firstDouble, secondDouble, result1);
 
             //Check if sum of double values is even using lambda expression
-            BiPredicate<Double, Double> chekIfEven2 = (num1, num2) ->DoublePredicateDemo.sumIsEven(firstDouble, secondDouble);
+            BiPredicate<Double, Double> chekIfEven2 = (num1, num2) ->BiPredicateDemo.sumIsEven(firstDouble, secondDouble);
             boolean result2 = chekIfEven2.test(firstDouble, secondDouble);
             System.out.printf("Using lambda expression: The sum of %.2f and %.2f is even: %b\n", firstDouble, secondDouble, result2);
 
             //Check if sum of double values is even using method referencing
-            BiPredicate<Double, Double> chekIfEven3 = DoublePredicateDemo::sumIsEven;
-            boolean result3 = chekIfEven2.test(firstDouble, secondDouble);
-            System.out.printf("Using method referencing: The sum of %.2f and %.2f is even: %b\n", firstDouble, secondDouble, result2);
-
+            BiPredicate<Double, Double> chekIfEven3 = BiPredicateDemo::sumIsEven;
+            boolean result3 = chekIfEven3.test(firstDouble, secondDouble);
+            System.out.printf("Using method referencing: The sum of %.2f and %.2f is even: %b\n", firstDouble, secondDouble, result3);
         }
     }
 }
