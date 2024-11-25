474205efa55726b319f995c63bd39aa74d741090
Yerfdog1990 <gouma308@gmail.com>
2024-11-25 19:55:20 +0700

    Creating Stream API using Stream builder.

---
+++ StreamAPI/src/Method4.java
@@ -0,0 +1,13 @@
+import java.util.stream.Stream;
+
+public class Method4 {
+    public static void main(String[] args) {
+        Stream<String> streamBuilder = Stream.<String>builder()
+                .add("Kenya")
+                .add("Uganda")
+                .add("Tanzania")
+                .add("Rwanda")
+                .add("Burundi").build();
+        streamBuilder.forEach(System.out::println);
+    }
+}
