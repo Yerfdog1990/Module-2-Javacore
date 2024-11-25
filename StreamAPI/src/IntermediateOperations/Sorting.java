87a75479fdb7138df1d9913323b4cd86baf8be6d
Yerfdog1990 <gouma308@gmail.com>
2024-11-26 06:16:37 +0700

    Filtering in streams

--- StreamAPI/src/IntermediateOperations/Sorting.java
+++ StreamAPI/src/IntermediateOperations/Sorting.java
@@ -12,6 +12,13 @@ public class Sorting {
         System.out.printf("Original List: %s%n", list);
 
         //Print sorted list
-        System.out.printf("Sorted List: %s%n", list.stream().sorted().collect(Collectors.toList()));
+        System.out.printf("Sorted List, natural order: %s%n", list.stream().sorted().collect(Collectors.toList()));
+        System.out.printf("Sorted List, reverse order: %s%n", list.stream().sorted((s1, s2) -> s2.compareTo(s1)).collect(Collectors.toList()));
+        System.out.printf("Sorted List, case insensitive: %s%n", list.stream().sorted(String.CASE_INSENSITIVE_ORDER).collect(Collectors.toList()));
+        System.out.printf("Sorted List, length ascending: %s%n", list.stream().sorted((s1, s2) -> s1.length() - s2.length()).collect(Collectors.toList()));
+        System.out.printf("Sorted List, length descending: %s%n", list.stream().sorted((s1, s2) -> s2.length() - s1.length()).collect(Collectors.toList()));
+        System.out.printf("Sorted List, alphabetical ascending: %s%n", list.stream().sorted().collect(Collectors.toList()));
+        System.out.printf("Sorted List, alphabetical descending: %s%n", list.stream().sorted((s1, s2) -> s2.compareTo(s1)).collect(Collectors.toList()));
+        System.out.printf("Sorted List, chained comparator: %s%n", list.stream().sorted(String.CASE_INSENSITIVE_ORDER.thenComparing(String.CASE_INSENSITIVE_ORDER)).collect(Collectors.toList()));
     }
 }
