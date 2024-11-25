9efb31f77280bc084427f0c14b6940dd9902de2a
Yerfdog1990 <gouma308@gmail.com>
2024-11-25 20:08:12 +0700

    Creating Stream API using using Stream.generate

--- StreamAPI/src/CreateStream/Method6.java
+++ StreamAPI/src/CreateStream/Method6.java
@@ -2,7 +2,7 @@ package CreateStream;
 
 import java.util.Random;
 import java.util.stream.Stream;
-
+//Creating an infinite Stream using `Stream.generate
 public class Method6 {
     public static void main(String[] args) {
         Stream<Integer> generate = Stream.generate(new Random()::nextInt).limit(10);
