d90645ae26713d7ad0ebdb993b22a25105538c7d
Yerfdog1990 <gouma308@gmail.com>
2024-11-25 19:47:35 +0700

    Creating Stream API from arrays.

--- StreamAPI/src/CreateStream/Method2.java
+++ StreamAPI/src/CreateStream/Method2.java
@@ -1,7 +1,7 @@
 package CreateStream;
 
 import java.util.Arrays;
-
+//Creating a Stream using "Arrays."
 public class Method2 {
     public static void main(String[] args) {
         String[] stringArray = {"Kenya", "Uganda", "Tanzania", "Rwanda", "Burundi"};
