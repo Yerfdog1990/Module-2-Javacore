5085355ed1a6d4ef00d0b76ef769b973daab0421
Yerfdog1990 <gouma308@gmail.com>
2024-11-25 19:48:18 +0700

    Creating Stream API from collections.

--- StreamAPI/src/CreateStream/Method1.java
+++ StreamAPI/src/CreateStream/Method1.java
@@ -2,7 +2,7 @@ package CreateStream;
 
 import java.util.Arrays;
 import java.util.List;
-
+//Creating a Stream using "collections."
 public class Method1 {
     public static void main(String[] args) {
         List<String> stringList = Arrays.asList("Kenya", "Uganda", "Tanzania", "Rwanda", "Burundi");
