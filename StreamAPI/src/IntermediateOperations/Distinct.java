e6e3e54139d888ac7a3e76a6a8f853d0a87bfbb1
Yerfdog1990 <gouma308@gmail.com>
2024-11-28 19:05:17 +0700

    Distinct operation in Stream API

--- StreamAPI/src/IntermediateOperations/Distinct.java
+++ StreamAPI/src/IntermediateOperations/Distinct.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Distinct {
     public static void main(String[] args) {
-        List<String> list = List.of("Kenya", "Uganda", "Tanzania", "Rwanda", "Burundi");
-        list.stream().distinct().forEach(System.out::println);
+        List<String> list = List.of("Kenya", "Uganda", "Burundi", "Tanzania", "Kenya", "Rwanda", "Burundi");
+        System.out.printf("Original List: %s%n", list);
+        System.out.printf("Distinct List: %s%n", list.stream().distinct().collect(java.util.stream.Collectors.toList()));
     }
 }
