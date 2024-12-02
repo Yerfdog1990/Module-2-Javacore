0d072e0b3ae1a2cf07078f3dec1a9e9e64becd04
Yerfdog1990 <gouma308@gmail.com>
2024-12-02 18:52:50 +0700

    Using non-volatile hides changes made by one thread from another thread.

---
+++ Multithreading/src/ThreadPriority/NonVolatile.java
@@ -0,0 +1,29 @@
+package ThreadPriority;
+
+public class NonVolatile {
+    private static boolean flag = false;
+
+    //Main method
+    public static void main(String[] args) {
+        Thread monitorThread = new Thread(()->{
+            while(flag){
+            }
+            System.out.println("Monitor thread has detected flag change!");
+        });
+        monitorThread.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Main flag is setting flag to false.");
+        flag = false;
+
+        /*
+        Explanation
+        The flag variable is not marked as volatile, so the monitor thread might use a cached value of flag instead of checking the shared memory.
+        The main thread updates flag to false, but the monitor thread might not detect this change and could continue looping indefinitely.
+         */
+    }
+}
