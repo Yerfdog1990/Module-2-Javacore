8892f5391be7aabc80be87cd8e4abd65fb709c1f
Yerfdog1990 <gouma308@gmail.com>
2024-12-09 20:39:39 +0700

    Calculate square of numbers using Callable and Future interfaces.

--- Executor/src/CallableandFuture/SquareNumbers.java
+++ Executor/src/CallableandFuture/SquareNumbers.java
@@ -1,10 +1,10 @@
 package CallableandFuture;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
+
 
 public class SquareNumbers implements Callable {
     private int number;
@@ -15,30 +15,44 @@ public class SquareNumbers implements Callable {
 
     @Override
     public Object call() throws Exception {
-        int result = number * number;
-        return result;
+        return number * number;
+    }
+    public static Future<Integer> calculateAsync(int number){
+        FutureTask<Integer> futureTask = new FutureTask<>(new SquareNumbers(number));
+        new Thread(futureTask).start();
+        return futureTask;
     }
 }
+
 class CalculateSquareNumbers{
     public static void main(String[] args) throws Exception{
         try(Scanner number = new Scanner(System.in)){
             System.out.print("Enter the number: ");
             int userInput = number.nextInt();
-            FutureTask<Integer> futureTask = new FutureTask<>(new SquareNumbers(userInput));
-            new Thread(futureTask).start();
-            int result;
-            if(futureTask.isDone()){
-                result = futureTask.get();
-            }else{
-                try{
-                    result = futureTask.get(100, TimeUnit.MILLISECONDS);
-                }catch (TimeoutException e){
-                    //Interrupt the task
-                    futureTask.cancel(true);
-                    System.out.println(userInput+ " * " +userInput+ " could not be computed within the allotted time. ");
-                }
+            // List to store Future results
+            List<Future<Integer>> futureList = new ArrayList<>();
+            // Submit tasks asynchronously
+            for (int i = 1; i <= userInput; i++) {
+                futureList.add(SquareNumbers.calculateAsync(i));
             }
-            System.out.println(userInput+ " * " +userInput+ " = " +futureTask.get());
+            // Print each result
+            for (int i = 1; i <= userInput; i++) {
+                Future<Integer> task = futureList.get(i-1);
+                Integer square;
+                if(task.isDone()){
+                    square = task.get();
+                }else{
+                    try{
+                        square = task.get(100, TimeUnit.SECONDS);
+                    }catch(TimeoutException e){
+                        //Cancel task
+                        task.cancel(true);
+                        System.out.println("Task cancelled as square of " +i+ " could not be calculated within the allotted time.");
+                        continue;
+                    }
+                }
+                System.out.println(i+ " * " +i+ " = " +square);
+            }
         }
     }
 }
