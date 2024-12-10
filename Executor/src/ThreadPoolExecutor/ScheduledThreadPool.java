8af5ee69afc50e92f2c9a30aa16b59ac1ea4b79b
Yerfdog1990 <gouma308@gmail.com>
2024-12-11 05:14:37 +0700

    Creates an executorService with scheduled thread pool.

--- Executor/src/ThreadPoolExecutor/ScheduledThreadPool.java
+++ Executor/src/ThreadPoolExecutor/ScheduledThreadPool.java
@@ -1,5 +1,21 @@
 package ThreadPoolExecutor;
 
-public class ScheduledThreadPool {
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+public class ScheduledThreadPool implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Checking email.....");
+    }
+
+    public static void main(String[] args) {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+        // Schedule the task to run periodically every 2 seconds, starting after a 1-second delay
+        executor.scheduleAtFixedRate(new ScheduledThreadPool(), 0, 5, TimeUnit.SECONDS);
+        //Shutdown after 10 seconds
+        executor.schedule(()->executor.shutdown(), 10, TimeUnit.SECONDS);
+    }
 }
