f74e1590e4b8b2c634caa86f5d17fda2ff5e5625
Godfrey Ouma <gouma308@gmail.com>
2025-01-06 04:31:48 +0700

    Get parameter type of a class using reflection API.

---
+++ ReflectionAPI/src/GetParameterType/Person.java
@@ -0,0 +1,38 @@
+package GetParameterType;
+
+import java.lang.reflect.TypeVariable;
+
+public class Person<T, U> {
+    private final T name;
+    private final U age;
+
+    //Constructor
+    public Person(U age, T name) {
+        this.age = age;
+        this.name = name;
+    }
+    public T getName() {
+        return name;
+    }
+    public U getAge() {
+        return age;
+    }
+    @Override
+    public String toString() {
+        return ("Name: " +name+
+                "\nAge: " +age);
+    }
+    //Main method
+    public static void main(String[] args) {
+        Class<?> personInstance = Person.class;
+        TypeVariable<?>[] typeParameters = personInstance.getTypeParameters();
+        System.out.println("Get a parameter type():");
+        for (TypeVariable<?> type : typeParameters) {
+            System.out.printf("Name: %s%n", type.getName());
+        }
+        System.out.println("\nGeneric parameter type():");
+        for (TypeVariable<?> type : typeParameters) {
+            System.out.printf("%s%n", type);
+        }
+    }
+}
