f9a3ddde4ea463613631937672308e9f1cfb28f5
Godfrey Ouma <gouma308@gmail.com>
2025-01-08 18:29:36 +0700

    Examine Socket class

--- Sockets/src/ExamineSocketClass/SocketClass.java
+++ Sockets/src/ExamineSocketClass/SocketClass.java
@@ -8,10 +8,17 @@ public class SocketClass {
     public static void main(String[] args) {
         Class<?> socketClass = Socket.class;
         System.out.printf("Name of the class: %s%n", socketClass.getName());
+        //Get constructors of socket class
+        Constructor<?>[] constructors = socketClass.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            System.out.printf("Constructor: %s%n", constructor.toString());
+        }
+        
         //Get methods of socket class
         Method[] methods = socketClass.getDeclaredMethods();
         for (Method method : methods) {
             System.out.printf("Method name: %s%n", method.getName());
         }
+        
     }
 }
