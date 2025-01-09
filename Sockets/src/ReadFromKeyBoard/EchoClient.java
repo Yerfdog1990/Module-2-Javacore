64f965ef1fa6c36d9ea5b7870a348bdc20b3e2d2
Godfrey Ouma <gouma308@gmail.com>
2025-01-09 18:05:51 +0700

    Implementing client socket

---
+++ Sockets/src/ReadFromKeyBoard/EchoClient.java
@@ -0,0 +1,25 @@
+package ReadFromKeyBoard;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class EchoClient {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Client started");
+            Socket clientSocket = new Socket("localhost", 6666);
+            System.out.println("Enter message:");
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            String message = userInput.readLine();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            out.println(message);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String response = in.readLine();
+            System.out.println(response);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
