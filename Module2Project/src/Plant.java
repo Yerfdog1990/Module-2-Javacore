e7f9a2a4b34fd868b64cc0b118a30cc2e6109af7
Godfrey Ouma <gouma308@gmail.com>
2025-01-11 15:40:46 +0700

    Day 3: Completed writing implementation of Plant class.

--- Module2Project/src/Plant.java
+++ Module2Project/src/Plant.java
@@ -1,5 +1,5 @@
 public class Plant {
-    private static final int MAX_PLANTS = 200;
+    public static final int MAX_PLANTS = 200;
 
     public static Plant grow() {
         if (Plant.MAX_PLANTS > 0) {
