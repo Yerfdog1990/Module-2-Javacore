e142b3f369d45122d33e5f1743c07c0c0efaf24e
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-23 07:49:30 +0700

    Creating threads by implementing Runnable interface

--- Multithreading/src/CreateThread/Method2.java
+++ Multithreading/src/CreateThread/Method2.java
@@ -1,12 +1,6 @@
 package CreateThread;
 
 public class Method2 implements Runnable{
-    @Override
-    public void run() {
-        System.out.println(Thread.currentThread().getName()+ " is running.");
-    }
-}
-class Main2{
     public static void main(String[] args) {
         int n = 10;
         for (int i = 0; i < n; i++) {
@@ -14,4 +8,9 @@ class Main2{
             object.run();
         }
     }
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+ " is running.");
+    }
 }
+
